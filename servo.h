e33a92f5f4e9d2d4771cd30436cdd61709c97469
jp112sdl <jp112sdl@icloud.com>
2019-12-31 15:56:05 +0100

    refactored servo code

--- servo.h
+++ servo.h
@@ -17,19 +17,28 @@
 #define SERVO_DUTY_MIN  2436UL //PWM Duty bei min. Stellwinkel
 #define SERVO_DUTY_MAX  8526UL //PWM Duty bei max. Stellwinkel
 
+unsigned long servoMoveStartMillis = 0;
+bool          servoPinIsAttached   = false;
+
 void setServoPos(uint8_t deg) {
+  servoMoveStartMillis = millis();
   uint32_t duty = map(deg, SERVO_DEG_MIN, SERVO_DEG_MAX, SERVO_DUTY_MIN, SERVO_DUTY_MAX);
+  ledcAttachPin(SERVO_PIN, SERVO_PWM_CH);
+  servoPinIsAttached = true;
   ledcWrite(SERVO_PWM_CH, duty);
+}
+
+void disableServo() {
+  if (servoPinIsAttached == true && millis() - servoMoveStartMillis > 1000) {
+    ledcDetachPin(SERVO_PIN);
+    servoPinIsAttached = false;
+  }
 }
 
 void initServo() {
   //ledcSetup(2, 50, 16); //channel, freq, resolution
   //ledcAttachPin(2, 2); // pin, channel
-
   ledcSetup(SERVO_PWM_CH, 50, 16);
-  ledcAttachPin(SERVO_PIN, SERVO_PWM_CH);
-
-  delay(100);
   setServoPos(90);
 }
 
